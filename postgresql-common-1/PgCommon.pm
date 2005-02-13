e7975e88c1d1ffabdbcd2bd55f1e1e92e8124971
Martin Pitt <martin@piware.de>
2005-02-13 21:29:09 +0000

    merged offline work on laptop branch

--- postgresql-common-1/PgCommon.pm
+++ postgresql-common-1/PgCommon.pm
@@ -6,14 +6,15 @@ package PgCommon;
 use Exporter;
 $VERSION = 1.00;
 @ISA = ('Exporter');
-@EXPORT = qw/error get_conf_value user_cluster_map get_program_path cluster_info get_versions get_version_clusters/;
-@EXPORT_OK = qw/$confroot $socket_dir/;
+@EXPORT = qw/error set_conf_value get_conf_value user_cluster_map
+    get_program_path cluster_info get_versions get_version_clusters
+    next_free_port/;
+@EXPORT_OK = qw/$confroot/;
 
 # configuration
 $mapfile = "/etc/postgresql-common/user_clusters";
 $confroot = "/etc/postgresql";
 $binroot = "/usr/lib/postgresql";
-$socketdir = "/var/run/postgresql";
 $defaultport = 5432;
 
 # Print an error message to stderr and exit with status 1
@@ -22,7 +23,7 @@ sub error {
     exit 1;
 }
 
-# Return parameter from a PostgreSQL configuration file, or '' if the parameter
+# Return parameter from a PostgreSQL configuration file, or undef if the parameter
 # does not exist.
 # Arguments: <version> <cluster> <parameter name>
 sub get_conf_value {
@@ -33,32 +34,34 @@ sub get_conf_value {
         }
         close F;
     }
-    return '';
+    return undef;
 }
 
-# Return the PostgreSQL version, cluster, and database to connect to. Return
-# ("","","") if $mapfile does not exist or has no entry for the current user
-sub user_cluster_map {
-    my ($user, $pwd, $uid, $gid) = getpwuid $>;
-    my $group = (getgrgid  $gid)[0];
+# Set parameter of a PostgreSQL configuration file.
+# Arguments: <version> <cluster> <parameter name> <value>
+sub set_conf_value {
+    my $fname = "$confroot/$_[0]/$_[1]/postgresql.conf";
 
-    if (! open MAP, $mapfile) {
-        print "Warning: could not open $mapfile, connecting to default port\n";
-        return ('','',$user);
+    # read configuration file lines
+    open (F, $fname) or die "Error: could not open $fname for reading";
+    push @lines, $_ while (<F>);
+    close F;
+
+    my $found = 0;
+    for ($i=0; $i <= $#lines; ++$i) {
+	if ($lines[$i] =~ /^\s*#?\s*$_[2]\s*=/) {
+	    $lines[$i] = "$_[2] = $_[3]\n";
+	    $found = 1;
+	}
     }
-    while (<MAP>) {
-        s/(.*?)#.*/$1/;
-        next if /^\s*$/;
-        ($u,$g,$v,$c,$db) = split;
-        if (!$db) {
-            print  "Warning: ignoring invalid line $. in $mapfile\n";
-            next;
-        }
-        if (($u eq "*" || $u eq $user) && ($g eq "*" || $g eq $group)) {
-            return ($v,$c, ($db eq "*") ? '' : $db);
-        }
+    push (@lines, "$_[2] = $_[3]\n") unless $found;
+
+    # write configuration file lines
+    open (F, '>'.$fname) or die "Error: could not open $fname for writing";
+    foreach (@lines) {
+	print F $_;
     }
-    return ('','','');
+    close F;
 }
 
 # Return the path of a program of a particular version.
@@ -67,6 +70,16 @@ sub get_program_path {
     my $path = "$binroot/$_[1]/bin/$_[0]";
     return $path if -x $path;
     return '';
+}
+
+# Check whether a postmaster server is running at the specified port.
+# Arguments: <version> <port>
+sub port_running {
+    $psql = get_program_path "psql", $_[0];
+    die "port_running: invalid port $_[1]" if $_[1] !~ /\d+/;
+    $out = `LANG=C $psql -p $_[1] -l 2>&1 > /dev/null`;
+    return 1 unless $?;
+    return (index ($out, "could not connect") < 0);
 }
 
 # Return a hash with information about a specific cluster.
@@ -78,7 +91,7 @@ sub cluster_info {
     $result{'pgdata'} = readlink $result{'configdir'} . "/pgdata";
     $result{'logfile'} = readlink $result{'configdir'} . "/log";
     $result{'port'} = (get_conf_value $_[0], $_[1], 'port') || $defaultport;
-    $result{'running'} = -S "$socketdir/.s.PGSQL." . $result{'port'};
+    $result{'running'} = port_running ($_[0], $result{'port'});
     ($result{'owneruid'}, $result{'ownergid'}) = 
         (stat $result{'pgdata'})[4,5];
     return %result;
@@ -108,4 +121,84 @@ sub get_version_clusters {
         closedir D;
     }
     return @clusters;
+}
+
+# Return the next free PostgreSQL port.
+sub next_free_port {
+    # create list of already used ports
+    for $v (get_versions) {
+	for $c (get_version_clusters $v) {
+	    $p = (get_conf_value $v, $c, 'port') || $defaultport;
+	    push @ports, $p;
+	}
+    }
+
+    for ($port = $defaultport; ; ++$port) {
+	last unless grep { $_ == $port } @ports;
+    }
+
+    return $port;
+}
+
+# Return the major server version that belongs to the given port. Return undef
+# if there is no cluster for this port.
+# Arguments: <port>
+sub port_version {
+    for $v (get_versions) {
+	for $c (get_version_clusters $v) {
+	    $p = (get_conf_value $v, $c, 'port') || $defaultport;
+	    return $v if $p == $_[0];
+	}
+    }
+
+    return undef;
+}
+
+# Return the PostgreSQL version, cluster, and database to connect to. version
+# is always set (defaulting to the version of the default port if no matching
+# entry is found), cluster and database may be 'undef'.
+sub user_cluster_map {
+    my ($user, $pwd, $uid, $gid) = getpwuid $>;
+    my $group = (getgrgid  $gid)[0];
+
+    # check per-user configuration file
+    $home = $ENV{"HOME"} || (getpwuid $>)[7];
+    $homemapfile = $home . '/.postgresqlrc';
+    if (open MAP, $homemapfile) {
+	while (<MAP>) {
+	    s/(.*?)#.*/$1/;
+	    next if /^\s*$/;
+	    ($v,$c,$db) = split;
+	    if ($db) {
+		close MAP;
+		return ($v, $c, ($db eq "*") ? undef : $db);
+	    } else {
+		print  "Warning: ignoring invalid line $. in $homemapfile\n";
+		next;
+	    }
+	}
+	close MAP;
+    }
+
+    # check global map file
+    if (! open MAP, $mapfile) {
+        print "Warning: could not open $mapfile, connecting to default port\n";
+        return (undef,undef,$user);
+    }
+    while (<MAP>) {
+        s/(.*?)#.*/$1/;
+        next if /^\s*$/;
+        ($u,$g,$v,$c,$db) = split;
+        if (!$db) {
+            print  "Warning: ignoring invalid line $. in $mapfile\n";
+            next;
+        }
+        if (($u eq "*" || $u eq $user) && ($g eq "*" || $g eq $group)) {
+	    close MAP;
+            return ($v,$c, ($db eq "*") ? undef : $db);
+        }
+    }
+    close MAP;
+
+    return (port_version $defaultport, undef, undef);
 }
