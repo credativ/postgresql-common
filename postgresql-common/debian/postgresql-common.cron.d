3592d11df9285ee916fad972c7f1a87a0aa80c47
unknown <martin@piware.de>
2005-10-26 09:22:02 +0200

    cronjob: check if pg_maintenance exists before calling it to avoid errors in "removed" state (#333803)

--- postgresql-common/debian/postgresql-common.cron.d
+++ postgresql-common/debian/postgresql-common.cron.d
@@ -12,10 +12,10 @@
 
 # Run VACUUM ANALYSE on all databases every 5 hours if pg_autovacuum is not
 # running
-2 0,5,10,15,20 * * 1-6 root /usr/sbin/pg_maintenance --analyze >/dev/null
+2 0,5,10,15,20 * * 1-6 root if [ -x /usr/sbin/pg_maintenance ]; then /usr/sbin/pg_maintenance --analyze >/dev/null; fi
 
 # On Sunday you may wish to run a VACUUM FULL ANALYSE as well
 # If you do not run a 24/7 site, you may want to uncomment the next line
 # so as to do a regular VACUUM FULL.  If you need 24/7 connectivity, save 
 # VACUUM FULL for when you think you really need it.
-# 10 3 * * Sun root /usr/sbin/pg_maintenance --full --analyze >/dev/null
+# 10 3 * * Sun root if [ -x /usr/sbin/pg_maintenance ]; then /usr/sbin/pg_maintenance --full --analyze >/dev/null; fi
