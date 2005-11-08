a06729d987f51be8dceafa2c266de7b4efad5862
unknown <martin@piware.de>
2005-11-08 00:33:35 -0500

    add useful test functions

--- postgresql-common/t/TestLib.pm
+++ postgresql-common/t/TestLib.pm
@@ -4,12 +4,53 @@
 package TestLib;
 use strict;
 use Exporter;
+use Test::More;
+
 our $VERSION = 1.00;
 our @ISA = ('Exporter');
-our @EXPORT = qw/ps/;
+our @EXPORT = qw/ps ok_dir exec_as @MAJORS $LATEST_MAJOR/;
+
+our @MAJORS = ('7.4', '8.0', '8.1');
+our $LATEST_MAJOR = $MAJORS[-1];
 
 # Return the user, group, and command line of running processes for the given
 # program.
 sub ps {
     return `ps h -o user,group,args -C $_[0] | grep '$_[0]' | sort -u`;
 }
+
+# Return an reference to an array of all entries but . and .. of the given directory.
+sub dircontent {
+    opendir D, $_[0] or die "opendir: $!";
+    my @e = grep { $_ ne '.' && $_ ne '..' } readdir (D);
+    closedir D;
+    return \@e;
+}
+
+# Check the contents of a directory.
+# Arguments: <directory name> <ref to expected dir content> <test description>
+sub ok_dir {
+    my $content = dircontent $_[0];
+    if (eq_set $content, $_[1]) {
+	pass $_[2];
+    } else {
+	diag "Expected directory contents: [@{$_[1]}], actual contents: [@$content]\n";
+	fail $_[2];
+    }
+}
+
+# Execute a command as a different user and return the output.
+# Arguments: <user> <system command> <ref to output>
+# Returns: Program exit code
+sub exec_as {
+    my $uid = getpwnam $_[0] or die "TestLib::exec_as: target user '$_[0]' does not exist";
+    $> = $uid;
+    die "changing euid: $!" if $!;
+    my $out = `$_[1] 2>&1`;
+    my $result = $? >> 8;
+    $> = 0;
+    die "changing euid back to root: $!" if $> != 0;
+    $_[2] = \$out;
+    return $result;
+}
+
