3d9fa94f4f678ba9d6ef368181484af0adffe703
unknown <martin@piware.de>
2006-02-11 12:11:09 +0100

    * debian/postgresql-common.cron.d: Update documentation for 8.1 and correct   paths in it. Closes: #351891

--- postgresql-common/debian/postgresql-common.cron.d
+++ postgresql-common/debian/postgresql-common.cron.d
@@ -5,10 +5,11 @@
 # default setting for new clusters created with pg_createcluster.
 #
 # If password access for local is turned on in
-# /etc/postgresql/<port>/pg_hba.conf, you must create a file 
+# /etc/postgresql/<version>/<cluster>/pg_hba.conf, you must create a file 
 # <cluster owner home directory>/.pgpass containing a line specifying
-# the password, as explained in the PostgreSQL Manual, section 27.12
-# (in package postgresql-doc-8.0).
+# the password, as explained in the PostgreSQL Manual, section 28.12
+# (if you have postgresql-doc-8.1 installed, you can find that part of
+# the manual in /usr/share/doc/postgresql-doc-8.1/html/libpq-pgpass.html).
 
 # Run VACUUM ANALYSE on all databases every 5 hours if pg_autovacuum is not
 # running
