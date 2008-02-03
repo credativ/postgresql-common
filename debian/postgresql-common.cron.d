abb73f29db159a3aad3141bc22f8e98d94c04f25
Martin Pitt <martin.pitt@ubuntu.com>
2008-02-03 13:16:19 +0100

    * debian/postgresql-common.cron.d: Update for 8.3, and add warnings and doc   pointer about enabling regular VACUUM FULL. (LP: #186831)

--- debian/postgresql-common.cron.d
+++ debian/postgresql-common.cron.d
@@ -7,16 +7,18 @@
 # If password access for local is turned on in
 # /etc/postgresql/<version>/<cluster>/pg_hba.conf, you must create a file 
 # <cluster owner home directory>/.pgpass containing a line specifying
-# the password, as explained in the PostgreSQL Manual, section 28.12
-# (if you have postgresql-doc-8.1 installed, you can find that part of
-# the manual in /usr/share/doc/postgresql-doc-8.1/html/libpq-pgpass.html).
+# the password, as explained in the PostgreSQL Manual, section 30.13
+# (if you have postgresql-doc-8.3 installed, you can find that part of
+# the manual in /usr/share/doc/postgresql-doc-8.3/html/libpq-pgpass.html).
 
 # Run VACUUM ANALYSE on all databases every 5 hours if pg_autovacuum is not
 # running
 2 0,5,10,15,20 * * 1-6 root if [ -x /usr/sbin/pg_maintenance ]; then /usr/sbin/pg_maintenance --analyze >/dev/null; fi
 
-# On Sunday you may wish to run a VACUUM FULL ANALYSE as well
-# If you do not run a 24/7 site, you may want to uncomment the next line
-# so as to do a regular VACUUM FULL.  If you need 24/7 connectivity, save 
-# VACUUM FULL for when you think you really need it.
+# In rare circumstances you might wish to regularly run VACUUM FULL
+# ANALYSE as well. This is not generally recommended, though, and
+# can have a drastic detrimental effect on database performance and
+# index size. Please read and understand 
+# /usr/share/doc/postgresql-doc-8.3/html/routine-vacuuming.html first
+# before enabling this.
 # 10 3 * * Sun root if [ -x /usr/sbin/pg_maintenance ]; then /usr/sbin/pg_maintenance --full --analyze >/dev/null; fi
