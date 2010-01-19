342438cc2dbd75854666ae9ad4af5e29b87ac7ad
Martin Pitt <mpitt@debian.org>
2010-01-19 22:42:50 +0100

    Add standard license headers to all programs, and update copyright.

--- t/TestLib.pm
+++ t/TestLib.pm
@@ -1,5 +1,16 @@
 # Common functionality for postgresql-common selftests
-# (c) 2005 Martin Pitt <mpitt@debian.org>
+#
+# (C) 2005-2009 Martin Pitt <mpitt@debian.org>
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
 
 package TestLib;
 use strict;
