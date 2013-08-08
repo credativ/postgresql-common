77f969c23258b8ca9dd4bc31d22963f2a90cbe6b
Christoph Berg <myon@debian.org>
2013-08-08 22:53:56 +0200

    pgxs_debian_control.mk: Drop the previously redundant grep check on debian/pgversions which would now break "X.Y+".

--- pgxs_debian_control.mk
+++ pgxs_debian_control.mk
@@ -8,12 +8,9 @@
 #
 debian/control: debian/control.in debian/pgversions
 	(set -e; \
-	VERSIONS=`pg_buildext supported-versions $(CURDIR)` || exit $$?; \
+	VERSIONS=`pg_buildext supported-versions $(CURDIR)`; \
 	grep-dctrl -vP PGVERSION $< > $@.pgxs_tmp; \
-	for v in $$VERSIONS; \
-        do                                         \
-		grep -q "^$$v" debian/pgversions   \
-		&& grep-dctrl -P PGVERSION $<      \
-		| sed -e "s:PGVERSION:$$v:" >> $@.pgxs_tmp; \
+	for v in $$VERSIONS; do \
+		grep-dctrl -P PGVERSION $< | sed -e "s:PGVERSION:$$v:" >> $@.pgxs_tmp; \
 	done; \
 	mv $@.pgxs_tmp $@) || (rm -f $@.pgxs_tmp; exit 1)
