a2c231d65d216d57138ef03de83b5f4c4a185b08
Christoph Berg <myon@debian.org>
2014-08-18 23:26:10 +0200

    Stop automatically updating debian/control from debian/control.in in pgxs_debian_control.mk. Instead, implement "checkcontrol" and "updatecontrol" actions in pg_buildext. checkcontrol is run from pgxs_debian_control.mk and will warn if an update is required. Backport and pgdg builds will still automatically update. (Closes: #758570)

--- pgxs_debian_control.mk
+++ pgxs_debian_control.mk
@@ -6,14 +6,8 @@
 # Author: Dimitri Fontaine <dfontaine@hi-media.com>
 #
 debian/control: debian/control.in debian/pgversions
-	(set -e; \
-	VERSIONS=`pg_buildext supported-versions $(CURDIR)`; \
-	grep-dctrl -vP PGVERSION $< > $@.pgxs_tmp; \
-	for v in $$VERSIONS; do \
-		grep-dctrl -P PGVERSION $< | sed -e "s:PGVERSION:$$v:" >> $@.pgxs_tmp; \
-	done; \
-	mv $@.pgxs_tmp $@) || (rm -f $@.pgxs_tmp; exit 1)
+	pg_buildext checkcontrol
 
-# Rebuild debian/control when clean is invoked
+# run check when clean is invoked
 clean: debian/control
 .PHONY: debian/control
