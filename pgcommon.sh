7be0dc09a65550c3be06b6545941ac3d60953afd
Christoph Berg <christoph.berg@credativ.de>
2019-09-03 13:18:22 +0200

    pgcommon.sh: Remove obsolete Ubuntu code in locale_gen function.

--- pgcommon.sh
+++ pgcommon.sh
@@ -34,55 +34,26 @@ get_release() {
     fi
 }
 
-# install locales; this happens differently on Debian and Ubuntu
+# install locales
 # Arguments: locale charset [...]
 locale_gen ()
 {
     get_release
-
     case $DISTRO in
-        debian)
-            METHOD=debian
-            ;;
-        ubuntu)
-            case $RELEASE in
-                12.*|13.*|14.*|15.*)
-                   METHOD=ubuntu
-                   ;;
-                *)
-                   METHOD=debian
-                   ;;
-            esac
-            ;;
         redhat|centos)
             return 0 # not yet implemented
             ;;
     esac
 
-    case $METHOD in
-        debian)
-            local run
-            while [ "${2:-}" ]; do
-                if ! grep -q "^$1 $2\$" /etc/locale.gen; then
-                    echo "$1 $2" >> /etc/locale.gen
-                    run=1
-                fi
-                shift 2
-            done
-            [ "${run:-}" ] && locale-gen
-            ;;
-
-        ubuntu)
-            # locale-gen will skip existing locales, so just call it
-            # unconditionally for all here
-            local locales
-            while [ "${2:-}" ]; do
-                locales="${locales:-} $1"
-                shift 2
-            done
-            locale-gen $locales
-            ;;
-    esac
+    local run
+    while [ "${2:-}" ]; do
+        if ! grep -q "^$1 $2\$" /etc/locale.gen; then
+            echo "$1 $2" >> /etc/locale.gen
+            run=1
+        fi
+        shift 2
+    done
+    [ "${run:-}" ] && locale-gen
 
     return 0
 }
